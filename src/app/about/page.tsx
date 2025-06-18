b9b76b523898597673de17101c3a78a57136177c
sangyoon <dltkddbs0810@softzion.com>
2025-06-18 17:38:37 +0900

    250618 fix: tailwind.config.ts 수정. sass 적용

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,24 +3,24 @@ export default function About() {
   return (
     <>
       <div className="min-h-screen bg-gray-50 p-4">
-      <header className="text-xl font-bold text-center mb-4">반응형 웹앱</header>
+        <header className="text-xl font-bold text-center mb-4">반응형 웹앱</header>
 
-      <main className="grid gap-4 
-                      grid-cols-1 
-                      sm:grid-cols-2 
-                      md:grid-cols-3 
-                      lg:grid-cols-4 
-                      xl:grid-cols-6">
-        {[...Array(12)].map((_, i) => (
-          <div
-            key={i}
-            className="bg-white shadow rounded-lg p-4 text-center"
-          >
-            카드 {i + 1}
-          </div>
-        ))}
-      </main>
-    </div>
+        <main className="grid gap-4 
+                        grid-cols-1 
+                        sm:grid-cols-2 
+                        md:grid-cols-3 
+                        lg:grid-cols-4 
+                        xl:grid-cols-6">
+          {[...Array(12)].map((_, i) => (
+            <div
+              key={i}
+              className="bg-white shadow rounded-lg p-4 text-center"
+            >
+              카드 {i + 1}
+            </div>
+          ))}
+        </main>
+      </div>
     </>
   )
 }
